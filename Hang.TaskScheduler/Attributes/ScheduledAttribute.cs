28427141bcb346c2fb03d6239667473a4f7d3153
Hang <yhnbgfd@gmail.com>
2017-03-23 10:43:50 +0800

    Add project files.

---
+++ Hang.TaskScheduler/Attributes/ScheduledAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hang.TaskScheduler.Attributes
+{
+    /// <summary>
+    /// 定时任务
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ScheduledAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Cron { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? FixedRate { get; private set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? FixedDelay { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cron"></param>
+        /// <param name="fixedRate"></param>
+        /// <param name="fixedDelay"></param>
+        public ScheduledAttribute(string cron, int? fixedRate = null, int? fixedDelay = null)
+        {
+            Cron = cron;
+            FixedRate = fixedRate;
+            FixedDelay = fixedDelay;
+        }
+    }
+}
