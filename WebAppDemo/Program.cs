02a09186b772ba2b3f29564e5bff6275119cfdf8
Hang <yhnbgfd@gmail.com>
2017-03-24 10:48:31 +0800

    <GeneratePackageOnBuild>True</GeneratePackageOnBuild>

--- WebAppDemo/Program.cs
+++ WebAppDemo/Program.cs
@@ -17,7 +17,6 @@ namespace WebAppDemo
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
