032b5084f572ce5963e625f8b56c514f4564f83f
Hang <yhnbgfd@gmail.com>
2017-08-18 12:00:31 +0800

    更新至.netcore2.0，添加WebAppDemo项目。

---
+++ WebAppDemo/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Hang.TaskScheduler.Base;
+using System.Diagnostics;
+
+namespace WebAppDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddHangTaskScheduler(options =>
+            {
+                options.AddCronTask(new Cron("* * * * *"), () => { Debug.WriteLine($"CronTask {DateTime.Now}"); });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseHangTaskScheduler();
+        }
+    }
+}
